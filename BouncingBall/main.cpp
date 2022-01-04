63d74c3cda9c2e560a147f331b279053bca377f5
whitestorm2346 <andrew770426@gmail.com>
2022-01-05 00:04:17 +0800

    Added a new Keyboard event

--- BouncingBall/main.cpp
+++ BouncingBall/main.cpp
@@ -1,43 +1,8 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
 #include <SFML/Graphics.hpp>
-
-#define winWidth 800
-#define winHeight 1000
-#define ballRadius 75
+#include "ball.hpp"
 
 #define getMousePosInWindow mousePosInWindow(window_Main.getPosition(), sf::Mouse::getPosition())
 #define getMousePosDisplacement mousePosDisplacement(mouseLastPos, mouseCurrPos)
-
-class Ball
-{
-public:
-    bool isCollide;
-    bool isDrag;
-    float velocity;
-    const float accelery = 5.f;
-    sf::CircleShape ball;
-
-    Ball()
-    {
-        isCollide = true;
-        isDrag = false;
-        velocity = 0;
-        ball.setRadius(ballRadius);
-        ball.setFillColor(sf::Color::White);
-        ball.setOrigin(ballRadius + 1, ballRadius + 1);
-        ball.setPosition(winWidth / 2, winHeight - ballRadius);
-    }
-    ~Ball(){}
-};
-
-sf::Vector2i mousePosInWindow(const sf::Vector2i&, const sf::Vector2i&);
-sf::Vector2i mousePosDisplacement(const sf::Vector2i&, const sf::Vector2i&);
-void setRandomColor(Ball&);
-void setBallPos(Ball&, sf::Vector2i);
-bool isInCircle(const sf::Vector2f, const sf::Vector2i);
 
 int main()
 {
@@ -46,7 +11,7 @@ int main()
     sf::Vector2i mouseCurrPos = getMousePosInWindow;
     sf::Vector2i dropping;
 
-    window_Main.setFramerateLimit(60);
+    window_Main.setFramerateLimit(90);
     Ball bouncingBall;
 
     while(window_Main.isOpen())
@@ -66,7 +31,7 @@ int main()
                     switch(event.key.code)
                     {
                         case sf::Mouse::Right:
-                            setRandomColor(bouncingBall);
+                            bouncingBall.setRandomColor();
 
                             break;
 
@@ -102,10 +67,26 @@ int main()
 
                     break;
 
+                case sf::Event::MouseWheelMoved:
+                    bouncingBall.setBallRadius(event.mouseWheel.delta);
+
+                    break;
+
+                case sf::Event::KeyPressed:
+                    switch(event.key.code)
+                    {
+                        case sf::Keyboard::Space:
+                            bouncingBall.velocity = -75;
+
+                            break;
+
+                        default: break;
+                    }
+
                 default: break;
             }
 
-            if(bouncingBall.isDrag) setBallPos(bouncingBall, getMousePosDisplacement);
+            if(bouncingBall.isDrag) bouncingBall.setBallPos(getMousePosDisplacement);
 
             mouseLastPos = mouseCurrPos;
         }
@@ -116,7 +97,7 @@ int main()
             dropping.x = 0;
             dropping.y = bouncingBall.velocity;
 
-            setBallPos(bouncingBall, dropping);
+            bouncingBall.setBallPos(dropping);
         }
 
         window_Main.clear(sf::Color::Black);
@@ -129,71 +110,3 @@ int main()
     return EXIT_SUCCESS;
 }
 
-sf::Vector2i mousePosInWindow(const sf::Vector2i& windowPos, const sf::Vector2i& mousePos)
-{
-    sf::Vector2i _mousePosInWindow;
-
-    _mousePosInWindow.x = mousePos.x - windowPos.x;
-    _mousePosInWindow.y = mousePos.y - windowPos.y;
-
-    return _mousePosInWindow;
-}
-sf::Vector2i mousePosDisplacement(const sf::Vector2i& lastPos, const sf::Vector2i& currPos)
-{
-    sf::Vector2i _mousePosDisplacement;
-
-    _mousePosDisplacement.x = currPos.x - lastPos.x;
-    _mousePosDisplacement.y = currPos.y - lastPos.y;
-
-    return _mousePosDisplacement;
-}
-void setRandomColor(Ball& obj)
-{
-    auto color = sf::Color{rand() % 206 + 50, rand() % 206 + 50, rand() % 206 + 50};
-
-    obj.ball.setFillColor(color);
-}
-void setBallPos(Ball& obj, sf::Vector2i displacement)
-{
-    sf::Vector2f objPos = obj.ball.getPosition();
-
-    objPos.x += displacement.x;
-    objPos.y += displacement.y;
-
-    if(objPos.x < ballRadius)
-    {
-        objPos.x = ballRadius;
-        //setRandomColor(obj);
-    }
-    else if(objPos.x > winWidth - ballRadius)
-    {
-        objPos.x = winWidth - ballRadius;
-        //setRandomColor(obj);
-    }
-
-    if(objPos.y < ballRadius)
-    {
-        objPos.y = ballRadius;
-        obj.isCollide = false;
-    }
-    else if(objPos.y > winHeight - ballRadius) // drop on floor
-    {
-        if(!obj.isCollide) setRandomColor(obj);
-
-        objPos.y = winHeight - ballRadius;
-        obj.isCollide = true;
-        obj.velocity = -(obj.velocity * 0.75f);
-
-        // if(abs(obj.velocity) < 0.001) obj.velocity = 0;
-    }
-    else obj.isCollide = false;
-
-    obj.ball.setPosition(objPos);
-}
-bool isInCircle(const sf::Vector2f objPos, const sf::Vector2i mousePos)
-{
-    double x_gap = abs(objPos.x - mousePos.x);
-    double y_gap = abs(objPos.y - mousePos.y);
-
-    return (x_gap * x_gap + y_gap * y_gap) < (ballRadius * ballRadius);
-}
