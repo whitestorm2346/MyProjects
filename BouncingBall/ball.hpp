ecee66f94164dff0f3a3e981c07177b32d3aec13
whitestorm2346 <andrew770426@gmail.com>
2022-01-05 00:04:51 +0800

    Added a header file

---
+++ BouncingBall/ball.hpp
@@ -0,0 +1,121 @@
+#ifndef BALL_HPP_INCLUDED
+#define BALL_HPP_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#define winWidth 800
+#define winHeight 1000
+
+float ballRadius = 100.f;
+
+class Ball
+{
+public:
+    bool isCollide;
+    bool isDrag;
+    float velocity;
+    const float accelery = 5.f;
+    sf::CircleShape ball;
+
+    Ball();
+    ~Ball(){}
+
+    void setRandomColor();
+    void setBallPos(sf::Vector2i);
+    void setBallRadius(int);
+};
+
+sf::Vector2i mousePosInWindow(const sf::Vector2i& windowPos, const sf::Vector2i& mousePos)
+{
+    sf::Vector2i _mousePosInWindow;
+
+    _mousePosInWindow.x = mousePos.x - windowPos.x;
+    _mousePosInWindow.y = mousePos.y - windowPos.y;
+
+    return _mousePosInWindow;
+}
+sf::Vector2i mousePosDisplacement(const sf::Vector2i& lastPos, const sf::Vector2i& currPos)
+{
+    sf::Vector2i _mousePosDisplacement;
+
+    _mousePosDisplacement.x = currPos.x - lastPos.x;
+    _mousePosDisplacement.y = currPos.y - lastPos.y;
+
+    return _mousePosDisplacement;
+}
+Ball::Ball()
+{
+    isCollide = true;
+    isDrag = false;
+    velocity = 0;
+    ball.setRadius(ballRadius);
+    ball.setFillColor(sf::Color::White);
+    ball.setOrigin(ballRadius + 1, ballRadius + 1);
+    ball.setPosition(winWidth / 2, winHeight - ballRadius);
+}
+void Ball::setRandomColor()
+{
+    auto color = sf::Color{
+        static_cast<unsigned char>(rand() % 206 + 50),
+        static_cast<unsigned char>(rand() % 206 + 50),
+        static_cast<unsigned char>(rand() % 206 + 50)
+    };
+
+    ball.setFillColor(color);
+}
+void Ball::setBallPos(sf::Vector2i displacement)
+{
+    sf::Vector2f objPos = ball.getPosition();
+
+    objPos.x += displacement.x;
+    objPos.y += displacement.y;
+
+    if(objPos.x < ballRadius)
+    {
+        objPos.x = ballRadius;
+        //setRandomColor();
+    }
+    else if(objPos.x > winWidth - ballRadius)
+    {
+        objPos.x = winWidth - ballRadius;
+        //setRandomColor();
+    }
+
+    if(objPos.y < ballRadius)
+    {
+        objPos.y = ballRadius;
+        isCollide = false;
+    }
+    else if(objPos.y > winHeight - ballRadius) // drop on floor
+    {
+        if(!isCollide) setRandomColor();
+
+        objPos.y = winHeight - ballRadius;
+        isCollide = true;
+        velocity = -(velocity * 0.75f);
+    }
+    else isCollide = false;
+
+    ball.setPosition(objPos);
+}
+bool isInCircle(const sf::Vector2f objPos, const sf::Vector2i mousePos)
+{
+    double x_gap = abs(objPos.x - mousePos.x);
+    double y_gap = abs(objPos.y - mousePos.y);
+
+    return (x_gap * x_gap + y_gap * y_gap) < (ballRadius * ballRadius);
+}
+void Ball::setBallRadius(int delta)
+{
+    ballRadius += static_cast<float>(delta);
+
+    ball.setRadius(ballRadius);
+    ball.setOrigin(ballRadius + 1, ballRadius + 1);
+    ball.setPosition(ball.getPosition().x, winHeight - ballRadius);
+}
+
+#endif // BALL_HPP_INCLUDED
