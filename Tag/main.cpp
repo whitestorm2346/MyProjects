2b86345b93fab108d926f21060e2a7a0d6bce678
whitestorm2346 <andrew770426@gmail.com>
2022-05-24 15:52:38 +0800

    Finished the field part and the BFS part

--- Tag/main.cpp
+++ Tag/main.cpp
@@ -9,17 +9,25 @@
 #define FIELD_WIDTH  30
 #define FIELD_HEIGHT 30
 
+#define OBSTACLE_COUNT_MIN 3
+#define OBSTACLE_COUNT_MAX 10
+
+#define OBSTACLE_SIZE_MIN 3
+#define OBSTACLE_SIZE_MAX 10
+
 #define FAST 'F'
 #define SLOW 'S'
 #define BOMB 'B'
 #define TURN 'T'
 
+// finished part
 template <typename Type> class Node; // doubly node
 template <typename Type> class List; // circular linked list
-
 class Obstacle;
 class Field;
 class Timer;
+
+// unfinished part
 class Distance;
 class Character;
 class Player;
@@ -30,12 +38,19 @@ class Game;
 
 template <typename Type>
 class Node{
-public:
-    Type data;
-    Node<Type>* prev;
-    Node<Type>* next;
+private:
+    Type _data_;
+    Node<Type>* _prev_;
+    Node<Type>* _next_;
 
+public:
     Node(Type data);
+
+    void setPrev(Node<Type>* prev);
+    void setNext(Node<Type>* next);
+    Type data();
+    Node<Type>* prev();
+    Node<Type>* next();
 };
 
 template <typename Type>
@@ -50,7 +65,8 @@ public:
     virtual ~List();
 
     void insert(Node<Type>* newNode);
-    void erase();
+    void pop_front();
+    void pop_back();
     bool empty();
     int  size();
 
@@ -69,23 +85,24 @@ public:
 
     void setSize(int width, int height);
     void setPosition(int x, int y);
+    std::pair<int, int> getSize();
+    std::pair<int, int> getPosition();
 };
 
 class Field{
 private:
-    std::pair<int, int> size;
     List<Obstacle*>* obstacles;
+    std::string matrix[FIELD_HEIGHT + 2];
 
 public:
-    char** matrix;
-
-    Field(int width, int height);
+    Field();
     virtual ~Field();
 
-    void setSize(int width, int height);
     void addObstacle();
     void generate();
     void print();
+    void setMatrix(int x, int y, char code);
+    char getMatrix(int x, int y);
 };
 
 class Timer{
@@ -104,15 +121,16 @@ public:
 
 class Distance{
 private:
-    std::pair<int, int> position;
+    std::pair<int, int> playerPosition;
     Field* field;
-    int** distance;
+    int distance[FIELD_HEIGHT][FIELD_HEIGHT];
 
 public:
-    Distance();
+    Distance(std::pair<int, int> playerPosition, Field* field);
     virtual ~Distance();
 
-    void setField();
+    void setPlayerPosition(std::pair<int, int> playerPosition);
+    void setField(Field* field);
     void countDistance();
 };
 
@@ -128,6 +146,7 @@ public:
     virtual ~Character();
 
     virtual void move() = 0;
+    virtual void action() = 0;
     virtual bool checkCollide();
 };
 
@@ -138,6 +157,7 @@ public:
     virtual ~Player();
 
     void move();
+    void action();
 };
 
 class Monster: public Character{
@@ -147,6 +167,7 @@ public:
     virtual ~Monster();
 
     void move();
+    void action();
 };
 
 class Item{
@@ -169,7 +190,6 @@ public:
 
 class Game{
 private:
-    std::pair<int, int> size;
     bool gameOver;
     Field* field;
     UserInterface* control;
@@ -186,9 +206,20 @@ void gotoxy(int x, int y);
 void hideCursor();
 
 int main(){
+    srand(time(nullptr));
+
+    Field* field = new Field();
+
+    field->generate();
+    field->print();
+
+    delete field;
+
+    /*
     Game game;
 
     game.run();
+    */
 
     return 0;
 }
@@ -211,11 +242,8 @@ void hideCursor(){
 }
 
 Game::Game(){
-    srand(time(nullptr));
-
-    size = {FIELD_WIDTH, FIELD_HEIGHT};
     gameOver = false;
-    field = new Field(size.first, size.second);
+    field = new Field();
     control = new UserInterface();
     characters = new List<Character*>();
 }
@@ -256,6 +284,9 @@ Monster::~Monster(){
 void Monster::move(){
 
 }
+void Monster::action(){
+
+}
 
 Player::Player(){
 
@@ -264,6 +295,9 @@ Player::~Player(){
 
 }
 void Player::move(){
+
+}
+void Player::action(){
 
 }
 
@@ -284,17 +318,64 @@ bool Character::checkCollide(){
     return true;
 }
 
-Distance::Distance(){
+Distance::Distance(std::pair<int, int> playerPosition, Field* field): playerPosition(playerPosition), field(field){
 
 }
 Distance::~Distance(){
 
 }
-void Distance::setField(){
-
+void Distance::setPlayerPosition(std::pair<int, int> playerPosition){
+    this->playerPosition = playerPosition;
+}
+void Distance::setField(Field* field){
+    this->field = field;
 }
 void Distance::countDistance(){
+    bool check[FIELD_HEIGHT][FIELD_WIDTH] = {};
 
+    List<std::pair<int, int>> que;
+
+    que.insert(new Node<std::pair<int, int>>({playerPosition.first, playerPosition.second}));
+    distance[playerPosition.second][playerPosition.first] = 0;
+
+    while(!que.empty()){
+        std::pair<int, int> currPos = que.getFront()->data();
+
+        que.pop_front();
+        check[currPos.second][currPos.first] = true;
+
+        // up
+        if(((!check[currPos.second - 1][currPos.first]) || (currPos.second - 1 >= 0)) &&
+            (field->getMatrix(currPos.first + 1, currPos.second) == ' ')){
+            check[currPos.second - 1][currPos.first] = true;
+            distance[currPos.second - 1][currPos.first] = distance[currPos.second][currPos.first] + 1;
+            que.insert(new Node<std::pair<int, int>>({currPos.first, currPos.second - 1}));
+        }
+
+        // right
+        if(((!check[currPos.second][currPos.first + 1]) || (currPos.first + 1 < FIELD_WIDTH)) &&
+            (field->getMatrix(currPos.first + 2, currPos.second + 1) == ' ')){
+            check[currPos.second][currPos.first + 1] = true;
+            distance[currPos.second][currPos.first + 1] = distance[currPos.second][currPos.first] + 1;
+            que.insert(new Node<std::pair<int, int>>({currPos.first + 1, currPos.second}));
+        }
+
+        // down
+        if(((!check[currPos.second + 1][currPos.first]) || (currPos.second + 1 < FIELD_HEIGHT)) &&
+            (field->getMatrix(currPos.first + 1, currPos.second + 2) == ' ')){
+            check[currPos.second + 1][currPos.first] = true;
+            distance[currPos.second + 1][currPos.first] = distance[currPos.second][currPos.first] + 1;
+            que.insert(new Node<std::pair<int, int>>({currPos.first, currPos.second + 1}));
+        }
+
+        // left
+        if(((!check[currPos.second][currPos.first - 1]) || (currPos.first - 1 >= 0)) &&
+            (field->getMatrix(currPos.first, currPos.second + 1) == ' ')){
+            check[currPos.second][currPos.first - 1] = true;
+            distance[currPos.second][currPos.first - 1] = distance[currPos.second][currPos.first] + 1;
+            que.insert(new Node<std::pair<int, int>>({currPos.first - 1, currPos.second}));
+        }
+    }
 }
 
 Timer::Timer(double timeGap): checkPoint(clock()), timeGap(timeGap){
@@ -313,53 +394,71 @@ bool Timer::exceedTimeGap(){
     return (static_cast<double>(clock() - checkPoint) / CLK_TCK >= timeGap);
 }
 
-Field::Field(int width, int height){
-    size = {width, height};
+Field::Field(){ // initialize the field
     obstacles = new List<Obstacle*>();
-    matrix = new char*[height];
 
-    for(int i = 0; i < height; i++){
-        matrix[i] = new char[width];
+    for(int i = 0; i < FIELD_WIDTH + 2; i++){
+        matrix[0] += '#';
+        matrix[FIELD_HEIGHT + 1] += '#';
+    }
 
-        for(int j = 0; j < width; j++){
-            matrix[i][j] = ' ';
+    for(int i = 1; i < FIELD_HEIGHT + 1; i++){
+        matrix[i] += '#';
+
+        for(int j = 1; j < FIELD_WIDTH + 1; j++){
+            matrix[i] += ' ';
         }
+
+        matrix[i] += '#';
     }
 }
 Field::~Field(){
-    for(int i = 0; i < size.second; i++){
-        delete [] matrix[i];
-    }
-
-    delete [] matrix;
     delete obstacles;
-}
-void Field::setSize(int width, int height){
-    size = {width, height};
 }
 void Field::addObstacle(){
     obstacles->insert(new Node<Obstacle*>(
         new Obstacle(
-            rand() % size.first,
-            rand() % size.second,
-            rand() % (size.first / 10) + 1,
-            rand() % (size.second / 10) + 1)
+            rand() % FIELD_WIDTH + 1, // x
+            rand() % FIELD_HEIGHT + 1, // y
+            rand() % (OBSTACLE_SIZE_MAX - OBSTACLE_SIZE_MIN + 1) + OBSTACLE_SIZE_MIN, // width
+            rand() % (OBSTACLE_SIZE_MAX - OBSTACLE_SIZE_MIN + 1) + OBSTACLE_SIZE_MIN) // height
         )
     );
 }
 void Field::generate(){
-    /**
-        wait to be implemented
-    */
+    for(int times = rand() % (OBSTACLE_COUNT_MAX - OBSTACLE_COUNT_MIN + 1) + OBSTACLE_COUNT_MIN; times >= 0; --times){
+        addObstacle();
+    }
+
+    Node<Obstacle*>* curr = obstacles->getFront();
+
+    for(int i = 0; i < obstacles->size(); i++, curr = curr->next()){
+        std::pair<int, int> size = curr->data()->getSize();
+        std::pair<int, int> position = curr->data()->getPosition();
+
+        for(int i = 0; i < size.second; i++){
+            for(int j = 0; j < size.first; j++){
+                int x = j + position.first + 1;
+                int y = i + position.second + 1;
+
+                if((x <= 0 || x >= FIELD_WIDTH + 1) ||
+                   (y <= 0 || y >= FIELD_HEIGHT + 1)) continue;
+
+                matrix[y][x] = '*';
+            }
+        }
+    }
 }
 void Field::print(){
-    for(int i = 0; i < size.second; i++){
-        for(int j = 0; j < size.second; j++){
-            std::cout<< matrix[i][j];
-        }
-
-        std::cout<< '\n';
+    for(int i = 0; i < FIELD_HEIGHT + 2; i++){
+        std::cout<< matrix[i] << '\n';
     }
+}
+void Field::setMatrix(int x, int y, char code){
+    matrix[y][x] = code;
+}
+char Field::getMatrix(int x, int y){
+    return matrix[y][x];
 }
 
 Obstacle::Obstacle(int x, int y, int width, int height){
@@ -375,13 +474,40 @@ void Obstacle::setSize(int width, int height){
 void Obstacle::setPosition(int x, int y){
     position = {x, y};
 }
+std::pair<int, int> Obstacle::getSize(){
+    return size;
+}
+std::pair<int, int> Obstacle::getPosition(){
+    return position;
+}
 
 template <typename Type>
 Node<Type>::Node(Type data){
-    this->data = data;
-    prev = nullptr;
-    next = nullptr;
+    this->_data_ = data;
+    _prev_ = nullptr;
+    _next_ = nullptr;
 }
+template <typename Type>
+void Node<Type>::setPrev(Node<Type>* prev){
+    _prev_ = prev;
+}
+template <typename Type>
+void Node<Type>::setNext(Node<Type>* next){
+    _next_ = next;
+}
+template <typename Type>
+Type Node<Type>::data(){
+    return _data_;
+}
+template <typename Type>
+Node<Type>* Node<Type>::prev(){
+    return _prev_;
+}
+template <typename Type>
+Node<Type>* Node<Type>::next(){
+    return _next_;
+}
+
 template <typename Type>
 List<Type>::List(){
     front = nullptr;
@@ -391,26 +517,48 @@ List<Type>::List(){
 template <typename Type>
 List<Type>::~List(){
     while(!empty()){
-        erase();
+        pop_back();
     }
 }
 template <typename Type>
 void List<Type>::insert(Node<Type>* newNode){
     if(nodeCount == 0){
         front = back = newNode;
-        newNode->next = newNode->prev = newNode;
+        newNode->setNext(newNode);
+        newNode->setPrev(newNode);
     }
     else{
-        front->prev = back->next = newNode;
-        newNode->next = front;
-        newNode->prev = back;
+        front->setPrev(newNode);
+        back->setNext(newNode);
+        newNode->setNext(front);
+        newNode->setPrev(back);
         back = newNode;
     }
 
     nodeCount++;
 }
 template <typename Type>
-void List<Type>::erase(){
+void List<Type>::pop_front(){
+    if(nodeCount <= 0) return;
+
+    Node<Type>* temp = front;
+
+    if(nodeCount == 1){
+        front = nullptr;
+        back = nullptr;
+    }
+    else{
+        back->setNext(front->next());
+        front->next()->setPrev(back);
+        front = front->prev();
+    }
+
+    nodeCount--;
+
+    delete temp;
+}
+template <typename Type>
+void List<Type>::pop_back(){
     if(nodeCount <= 0) return;
 
     Node<Type>* temp = back;
@@ -420,9 +568,9 @@ void List<Type>::erase(){
         back = nullptr;
     }
     else{
-        front->prev = back->prev;
-        back->prev->next = front;
-        back = back->prev;
+        front->setPrev(back->prev());
+        back->prev()->setNext(front);
+        back = back->prev();
     }
 
     nodeCount--;
