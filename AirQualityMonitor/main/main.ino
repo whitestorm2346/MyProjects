72f989690a69f4b5d264db4593789d3f5ac6346b
whitestorm2346 <andrew770426@gmail.com>
2022-11-27 16:17:09 +0800

    Added ISD1820.h into this project

--- AirQualityMonitor/main/main.ino
+++ AirQualityMonitor/main/main.ino
@@ -1,17 +1,19 @@
 #include <Adafruit_PM25AQI.h>
 #include <SimpleDHT.h>
 #include <IRremote.h>
-#include <ISD1820.h>
-#include <Adafruit_LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include "ISD1820.h"
 
 // object pin
 #define PM25_PIN 0
 #define DHT_PIN 1
 #define MQ9_PIN 2
 #define RECV_PIN 3
-#define ISD_PIN 4
-#define LCD_PIN 5
-#define LED_PIN 6
+#define ISD_REC_PIN 4
+#define ISD_PLAY_E_PIN 5
+#define ISD_PLAY_L_PIN 6
+#define LCD_PIN 7
+#define LED_PIN 8
 
 // remote control decode
 #define REPORT_ALL 0
@@ -19,42 +21,75 @@
 #define REPORT_DHT 2
 #define REPORT_GAS 3
 
-// MQ9 init
+// PM25 init
+Adafruit_PM25AQI pm25(PM25_PIN);
 
 // DHT init
+SimpleDHT dht(DHT_PIN);
+
+// MQ9 init
 
 // IRremote init
 IRrecv irrecv(RECV_PIN);
 decode_results result;
 
 // ISD1820 init
+ISD1820 isd(ISD_REC_PIN, ISD_PLAY_E_PIN, ISD_PLAY_L_PIN);
 
 // IC_LCD init
-Adafruit_LiquidCrystal LCD(0);
+LiquidCrystal_I2C LCD(0x27, 16, 2);
 
+void report(int code);
+void print(int code);
+
+// module initial part
 void setup() {
-  LCD.begin(16, 2);
+  LCD.init();
+  LCD.backlight();
   irrecv.enableIRIn();
 }
 
+// main loop
 void loop() {
   if (irrecv.decode(&result)) {
-    switch (result.value) {
-      case REPORT_ALL:
-        break;
-
-      case REPORT_PM25:
-        break;
-
-      case REPORT_DHT:
-        break;
-
-      case REPORT_GAS:
-        break;
-
-      default: break;
-    }
+    report(result.value);
 
     irrecv.resume();
+  }
+}
+
+void report(int code){
+  switch(code){
+    case REPORT_ALL:
+        break;
+
+    case REPORT_PM25:
+      break;
+
+    case REPORT_DHT:
+      break;
+
+    case REPORT_GAS:
+      break;
+
+    default: break;
+  }
+}
+
+void print(int code){
+  switch(code){
+    case REPORT_ALL:
+        break;
+
+    case REPORT_PM25:
+      break;
+
+    case REPORT_DHT:
+      break;
+
+    case REPORT_GAS:
+      break;
+
+    default: break;
   }
 }
